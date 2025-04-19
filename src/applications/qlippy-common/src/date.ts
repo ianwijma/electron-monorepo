2b992cd9748995db8e6f230433d6a8524f4a8842
ianwijma <ian@wij.ma>
2025-04-19 22:15:35 +1000

    Fixed days, weeks, months and years being used incorrectly.

--- src/applications/qlippy-common/src/date.ts
+++ src/applications/qlippy-common/src/date.ts
@@ -9,18 +9,20 @@ export const toHumanDateAgo = (dateTimestamp: number): string => {
     const differenceInMonths = now.diff(date, 'months');
     const differenceInYears = now.diff(date, 'years');
 
+    const s = (text: string, count: number) => count === 1 ? text : `${text}s`;
+
     if (differenceInDays <= 0) {
         return 'Today';
     } else if (differenceInDays === 1) {
         return 'Yesterday';
     } else if (differenceInDays > 1 && differenceInWeeks <= 0) {
-        return `${differenceInDays} days ago`;
+        return `${differenceInDays} ${s('day', differenceInDays)} ago`;
     } else if (differenceInWeeks > 0 && differenceInMonths <= 0) {
-        return `${differenceInWeeks} weeks ago`;
+        return `${differenceInWeeks} ${s('week', differenceInWeeks)} ago`;
     } else if (differenceInMonths > 0 && differenceInYears <= 0) {
-        return `${differenceInMonths} months ago`;
+        return `${differenceInMonths} ${s('month', differenceInMonths)} ago`;
     } else if (differenceInYears > 0 && differenceInYears < 10) {
-        return `${differenceInYears} years ago`;
+        return `${differenceInYears} ${s('year', differenceInYears)} ago`;
     } else {
         // More than a decade...
         return 'Wow...'
