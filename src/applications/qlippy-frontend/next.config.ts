aa48d1a4f385468fefcb90c50b6f06086177f3c7
Ian Wijma <iwijma@atlassian.com>
2025-04-22 22:44:20 +1000

    Fixed MacOS build

--- src/applications/qlippy-frontend/next.config.ts
+++ src/applications/qlippy-frontend/next.config.ts
@@ -1,11 +1,17 @@
+import { platform } from 'node:os'
 import type {NextConfig} from "next";
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV === 'production'
+const isMac = platform() === 'darwin'
+
+const LINUX_PRODUCTION_BASE_PATH = '/usr/lib/qlippy/resources/app.asar/.webpack/renderer';
+const MAC_PRODUCTION_BASE_PATH = '/Applications/qlippy-backend.app/Contents/Resources/app.asar/.webpack/renderer';
+const PRODUCTION_BASE_PATH = isMac ? MAC_PRODUCTION_BASE_PATH : LINUX_PRODUCTION_BASE_PATH;
 
 const nextConfig: NextConfig = {
     /* config options here */
     output: 'export',
-    basePath: isProd ? '/usr/lib/qlippy/resources/app.asar/.webpack/renderer' : '',
+    basePath: isProduction ? PRODUCTION_BASE_PATH : '',
 };
 
 export default nextConfig;
